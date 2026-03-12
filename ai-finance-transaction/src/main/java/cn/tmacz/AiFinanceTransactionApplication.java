8b0cc967360fe54ddc8f1c93b113cba10e36b63d
我爱大西瓜 <2466721401@qq.com>
2026-03-12 23:29:12 +0800

    声明依赖

---
+++ ai-finance-transaction/src/main/java/cn/tmacz/AiFinanceTransactionApplication.java
@@ -0,0 +1,13 @@
+package cn.tmacz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinanceTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFinanceTransactionApplication.class, args);
+    }
+
+}
