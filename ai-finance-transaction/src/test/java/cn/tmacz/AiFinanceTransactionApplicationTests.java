bed87f7f089e3b299884228bd10b6966cf23f0fb
我爱大西瓜 <2466721401@qq.com>
2026-03-14 00:04:09 +0800

    包依赖管理

---
+++ ai-finance-transaction/src/test/java/cn/tmacz/AiFinanceTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+//package cn.tmacz;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AiFinanceTransactionApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
